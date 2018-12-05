985c5760f28bce6c7842fd322a601d63320b364a
Miinivi <venianelam@gmail.com>
2018-12-05 10:27:30 +0100

    ajout de l'index.php et du header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+<title>Gestion appli frais </title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link real="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+/*
+  $bdd = mysqli_connect('localhost', 'root', '', 'nomdebddpasencorechoisi');
+*/
+?>
