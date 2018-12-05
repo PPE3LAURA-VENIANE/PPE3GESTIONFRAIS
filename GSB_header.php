4f00b35ee2499c41ce9212dfcf65f93434b3605a
Miinivi <venianelam@gmail.com>
2018-12-05 11:09:49 +0100

    généralités

--- GSB_header.php
+++ GSB_header.php
@@ -4,7 +4,7 @@
 <title>Gestion appli frais </title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-<link real="stylesheet" href="style.css">
+<link real="stylesheet" href="GSB_style.css">
 </head>
 <body>
 
